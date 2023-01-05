8d8b7ee0363b1602b3ba88d7cc28d332dce5f8c7
Grigory <gregory.rvl@yandex.ru>
2023-01-05 18:12:42 +0300

    second commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import Logic from "./logic";
+
+const field = document.querySelector('.field-container');
+const logic = new Logic(field);
+
+logic.drawBoard();
+logic.moveGoblin();
---
+++ src/js/logic.js
@@ -0,0 +1,37 @@
+export default class Logic {
+  constructor(field) {
+    this.field = field;
+  };
+
+  drawBoard() {
+    for (let i = 0; i < 16; i += 1) {
+      const newCell = '<div class="cell"></div>';
+      this.field.innerHTML += newCell; 
+    };
+  };
+
+  randomizeCell(index) {
+    const result = Math.floor(Math.random() * (this.field.childNodes.length - 1));
+
+    if (result === index) {
+      return this.randomizeCell(index);
+    };
+
+    return result;
+  };
+
+  moveGoblin() {
+    let index = 0;
+
+    setInterval(() => {
+      const activeCell = this.field.querySelector('.active');
+
+      if(activeCell) {
+        activeCell.classList.remove('active');
+      };
+      index = this.randomizeCell(index);
+      const newCell = this.field.childNodes[index];
+      newCell.classList.add('active');
+    }, 2000);
+  };
+}
